f5267bd8bbee0146a105b059a9252ad46a4026d8
i-rme <rme@rme.li>
2019-10-03 17:52:53 +0200

    Added layer 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
 import stack.Layer1;
+import stack.Layer2;
 
 public class Main {
 	public static void main(String[] args) {
-		new Layer1().run();
+		//new Layer1().run();
+		new Layer2().run();
 	}
 }
--- src/stack/Layer.java
+++ src/stack/Layer.java
@@ -1,6 +1,7 @@
 package stack;
 
 public abstract class Layer extends Thread {
-	public abstract void config();
+	public Layer above, below;
+	public abstract void config(Layer above, Layer below);
 	public abstract void run();
 }
