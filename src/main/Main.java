ca563ef53f9f58584199b947096c138dfa8527e2
Rodrigo Casamayor <rodrigo.casamayor@gmail.com>
2019-10-08 14:40:22 +0200

    A few more changes to meet the requirements of Part 2.
    1. Creation of CustomPacket class
    2. Minor changes of Main and Layer classes.
    3. Deletion of ReceivePackets class.

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		Layer1 physical = new Layer1();
 		Layer2 datalink = new Layer2();
-		
-		physical.setTopLayer(datalink);
-		datalink.setLowLayer(physical);
-		
+
+		physical.setNeighbourLayers(datalink, null);
+		datalink.setNeighbourLayers(null, physical);
+
 		physical.run();
 		datalink.run();
 	}
--- src/stack/Layer.java
+++ src/stack/Layer.java
@@ -25,49 +25,58 @@ import jpcap.packet.Packet;
 public abstract class Layer extends Thread {
 	Layer topLayer;
 	Layer lowLayer;
-	
+
 	Queue<Packet> topQueue = new LinkedList<Packet>();
 	Queue<Packet> lowQueue = new LinkedList<Packet>();
-	
-	Semaphore topSemaphore = new Semaphore(1);  // only 1 thread can access
-	Semaphore lowSemaphore = new Semaphore(1);  // the resource at any one time.
-	
+
+	Semaphore topSemaphore = new Semaphore(1); // only 1 thread can access
+	Semaphore lowSemaphore = new Semaphore(1); // the resource at any one time.
+
 	public abstract void config();
+
 	public abstract void run();
-	
+
 	public void sendUpwards(Packet p) {
 		try {
 			topLayer.lowSemaphore.acquire();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		topLayer.lowQueue.add(p);
-		
+
 		topLayer.lowSemaphore.release();
 	}
-	
+
 	public void sendDownwards(Packet p) {
 		try {
 			lowLayer.topSemaphore.acquire();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		lowLayer.topQueue.add(p);
-		
+
 		lowLayer.topSemaphore.release();
 	}
-	
+
+	public void setNeighbourLayers(Layer topLayer, Layer lowLayer) {
+		this.topLayer = topLayer;
+		this.lowLayer = lowLayer;
+	}
+
 	public Layer getTopLayer() {
 		return topLayer;
 	}
+
 	public void setTopLayer(Layer topLayer) {
 		this.topLayer = topLayer;
 	}
+
 	public Layer getLowLayer() {
 		return lowLayer;
 	}
+
 	public void setLowLayer(Layer lowLayer) {
 		this.lowLayer = lowLayer;
 	}
