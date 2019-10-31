0a31a88a99f44201bf77bfbb6fd950d11665aa91
Rodrigo Casamayor <rodrigo.casamayor@gmail.com>
2019-10-31 17:46:41 +0100

    Implemented ProtocolARP and ProtocolIP

--- src/stack/ProtocolARP.java
+++ src/stack/ProtocolARP.java
@@ -1,8 +1,23 @@
 package stack;
 
+import jpcap.packet.ARPPacket;
+
 public class ProtocolARP extends Protocol {
 	@Override
 	public void run() {
-
+		while (true) {
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (!packets.isEmpty()) {
+				ARPPacket arpPacket = (ARPPacket) packets.poll();
+				System.out.println("ARP PACKET: " + arpPacket);	
+			} else {
+				semaphore.release();
+			}
+		}
 	}
 }
--- src/stack/ProtocolIP.java
+++ src/stack/ProtocolIP.java
@@ -1,8 +1,23 @@
 package stack;
 
+import jpcap.packet.IPPacket;
+
 public class ProtocolIP extends Protocol {
 	@Override
 	public void run() {
-
+		while (true) {
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (!packets.isEmpty()) {
+				IPPacket ipPacket = (IPPacket) packets.poll();
+				System.out.println("IP PACKET: " + ipPacket);
+			} else {
+				semaphore.release();
+			}
+		}
 	}
 }
